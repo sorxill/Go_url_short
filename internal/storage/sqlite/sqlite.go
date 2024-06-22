c151f69f2c780a4932bb19e4ccc0efd177b0b132
Yaroslav <yarikbritov@yandex.ru>
2024-06-22 23:32:55 +0300

    feat: create saveURL command - command for save short and full url to db / add constaint for this table

--- internal/storage/sqlite/sqlite.go
+++ internal/storage/sqlite/sqlite.go
@@ -3,8 +3,10 @@ package sqlite
 import (
 	"database/sql"
 	"fmt"
+	"go_url_short/internal/storage"
 
-	_ "github.com/mattn/go-sqlite3" // init sqlite3 driver
+	"github.com/mattn/go-sqlite3"
+	// init sqlite3 driver
 )
 
 type Storage struct {
@@ -39,4 +41,24 @@ func New(storage_path string) (*Storage, error) {
 	}
 
 	return &Storage{db: db}, nil
+}
+
+func (s *Storage) SaveURL(urlToSave string, alias string) error {
+	const op = "storage.sqlite.SaveURL"
+
+	stmt, err := s.db.Prepare("INSERT INTO url(url, alias) VALUES(?, ?)")
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.Exec(urlToSave, alias)
+	if err != nil {
+		if sqliteErr, ok := err.(sqlite3.Error); ok && sqliteErr.ExtendedCode == sqlite3.ErrConstraintUnique {
+			return fmt.Errorf("%s: %w", op, storage.ErrURLExists)
+		}
+
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
 }
